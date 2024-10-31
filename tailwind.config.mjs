6100b26da456853cf9981507415981be1ce6f626
vibegrith <vibe9737@stud.kea.dk>
2024-10-31 11:07:56 +0100

    sektion7

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,10 +13,27 @@ export default {
           "0%, 100%": { transform: "translateY(0)" },
           "50%": { transform: "translateY(-20px)" },
         },
+        slideshow: {
+          "0%, 33%": { opacity: "1" }, // Første billede vises i starten
+          "33.33%, 66%": { opacity: "0" }, // Andet billede vises i midten
+          "66.66%, 100%": { opacity: "0" }, // Tredje billede vises til sidst
+        },
+        slideshow2: {
+          "0%, 33%": { opacity: "0" },
+          "33.33%, 66%": { opacity: "1" }, // Andet billede vises i midten
+          "66.66%, 100%": { opacity: "0" },
+        },
+        slideshow3: {
+          "0%, 66%": { opacity: "0" },
+          "66.66%, 100%": { opacity: "1" }, // Tredje billede vises til sidst
+        },
       },
       animation: {
         "progress-fill": "progress-fill 3s ease forwards", // Animation over 3 sekunder
         swim: "swim 3s ease-in-out infinite",
+        slideshow1: "slideshow 9s ease-in-out infinite",
+        slideshow2: "slideshow2 9s ease-in-out infinite",
+        slideshow3: "slideshow3 9s ease-in-out infinite",
       },
     },
   },
